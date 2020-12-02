a87a8722ed79aec81099f11af2eca3c806977f92
Richard Carter <richard@richardcarter.org>
2020-12-02 13:48:26 -0600

    day 1 problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("need to pass input filename");
+    }
+    let input_filename = &args[1];
+    let input = fs::read_to_string(input_filename)
+        .expect("Something went wrong loading input file");
+    let mut numbers: Vec<i32> = input.lines()
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    numbers.sort_unstable();
+
+    let mut low_idx = 0;
+    let mut high_idx = numbers.len() - 1;
+
+    while low_idx < high_idx {
+        let sum = numbers[low_idx] + numbers[high_idx];
+
+        if sum == 2020 {
+            println!("{:?}", numbers[low_idx] * numbers[high_idx]);
+            std::process::exit(0);
+        } else if sum < 2020 {
+            low_idx += 1;
+        } else /* if sum > 2020 */ {
+            high_idx -= 1;
+        }
+    }
+
+    println!("{:?}", numbers);
 }
+
