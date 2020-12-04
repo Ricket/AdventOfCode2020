490d5cd673ecb43e66db5f834003e3759ec1dfa9
Richard Carter <richard@richardcarter.org>
2020-12-03 18:40:46 -0600

    day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,31 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        panic!("need to pass input filename");
+    if args.len() != 4 {
+        panic!("Args: day part inputfile");
     }
-    let input_filename = &args[1];
+    let day = &args[1];
+    let part = &args[2];
+    let input_filename = &args[3];
     let input = fs::read_to_string(input_filename)
         .expect("Something went wrong loading input file");
+
+    day1part2(input);
+}
+
+fn sorted_int_vec(input: String) -> Vec<i32> {
     let mut numbers: Vec<i32> = input.lines()
         .map(|s| s.trim())
         .filter(|s| !s.is_empty())
         .map(|s| s.parse().unwrap())
         .collect();
-
     numbers.sort_unstable();
+    numbers
+}
+
+fn day1part1(input: String) {
+    let numbers = sorted_int_vec(input);
+    // println!("{:?}", numbers);
 
     let mut low_idx = 0;
     let mut high_idx = numbers.len() - 1;
@@ -33,6 +45,24 @@ fn main() {
         }
     }
 
-    println!("{:?}", numbers);
 }
 
+fn day1part2(input: String) {
+    let numbers = sorted_int_vec(input);
+
+    if numbers.len() < 3 {
+        panic!("Less than 3 input numbers");
+    }
+
+    for low in 0..numbers.len() {
+        for med in low..numbers.len() {
+            for high in med..numbers.len() {
+                let sum = numbers[low] + numbers[med] + numbers[high];
+                if sum == 2020 {
+                    println!("{:?}", numbers[low] * numbers[med] * numbers[high]);
+                    std::process::exit(0);
+                }
+            }
+        }
+    }
+}
